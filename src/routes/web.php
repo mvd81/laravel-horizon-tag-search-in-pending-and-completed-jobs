60586b6b7cb93a115281e5a35ec10fe4e23c5d57
Marcel van Doornen <vandoornenmarcel@gmail.com>
2024-11-13 11:34:58 +0100

    Refactor route definitions for Horizon views and jobs
    Consolidate the route definitions for Horizon views and job controllers under a single namespace. Add a prefix to the job routes for better API structuring and maintain the middleware and domain configurations.

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,23 +5,21 @@ use mvd81\laravelHorizonTagSearchInPendingAndCompletedJobs\Http\Controllers\Comp
 use mvd81\laravelHorizonTagSearchInPendingAndCompletedJobs\Http\Controllers\CustomHomeController;
 use mvd81\laravelHorizonTagSearchInPendingAndCompletedJobs\Http\Controllers\PendingJobsController;
 
-Route::middleware(config('horizon.middleware', 'web'))
-    ->namespace($this->app->getNamespace().'Http\Controllers')
-    ->group(function () {
-        Route::get('/horizon/{view?}', [CustomHomeController::class, 'index'])
-            ->where('view', '(.*)')
-            ->name('horizon.index');
-    });
-
 Route::namespace('mvd81\laravelHorizonTagSearchInPendingAndCompletedJobs\Http\Controllers')
     ->domain(config('horizon.domain', null))
     ->prefix(config('horizon.path'))
     ->middleware(config('horizon.middleware', 'web'))
     ->group(function () {
 
-        Route::get('/jobs/pending', [PendingJobsController::class, 'index'])
+        Route::get('/horizon/{view?}', [CustomHomeController::class, 'index'])
+            ->where('view', '(.*)')
+            ->name('horizon.index');
+
+        Route::prefix('api')
+            ->get('/jobs/pending', [PendingJobsController::class, 'index'])
             ->name('horizon.pending-jobs.index');
 
-        Route::get('/jobs/completed', [CompletedJobsController::class, 'index'])
+        Route::prefix('api')
+            ->get('/jobs/completed', [CompletedJobsController::class, 'index'])
             ->name('horizon.completed-jobs.index');
     });
